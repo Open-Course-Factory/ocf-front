d64cf86be73388ce69136ef4e107c585a1f34bb0
Julien Damiano <144369521+Hooch64@users.noreply.github.com>
2024-06-05 13:43:13 +0200

    Added Guacamole client and dependecies (WIP)

--- src/components/Pages/Dashboard.vue
+++ src/components/Pages/Dashboard.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, onBeforeMount } from 'vue';
-import { useRoute } from 'vue-router';
-import { useCurrentUserStore } from '../../store/currentUser.ts';
-import axios from 'axios'
-import { useAxios } from '../../composables/useAxios.ts';
-import Disconnect from '../Buttons/Disconnect.vue';
+
 import MainNavMenu from '../Menus/MainNavMenu.vue';
 import TopMenu from '../Menus/TopMenu.vue';
 
--- src/components/Pages/Tps.vue
+++ src/components/Pages/Tps.vue
@@ -2,6 +2,7 @@
 import MainNavMenu from '../Menus/MainNavMenu.vue';
 import TopMenu from '../Menus/TopMenu.vue';
 import StartSession from '../Buttons/StartSession.vue';
+import GuacConnect from '../Guacamole/GuacConnect.vue'
 </script>
 
 <template>
@@ -10,6 +11,8 @@ import StartSession from '../Buttons/StartSession.vue';
         <div class="inner-wrapper">
             <TopMenu />
             <StartSession />
+            <GuacConnect />
+
         </div>
     </div>
 </template>
