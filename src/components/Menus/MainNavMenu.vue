23f857d838622a1904a4092c60a40742b18fa2a6
Julien Damiano <144369521+Hooch64@users.noreply.github.com>
2024-06-10 14:06:46 +0200

    CSS fix

--- src/components/Menus/MainNavMenu.vue
+++ src/components/Menus/MainNavMenu.vue
@@ -38,4 +38,31 @@ p {
   text-transform: uppercase;
 }
 
+a {
+  color: #e7e7e7;
+  text-decoration: none;
+}
+
+h1 a {
+  font-size: 26px;
+  margin-left: 20px;
+}
+
+ul {
+  list-style-type: none;
+  margin-left: -32px;
+}
+
+ul li a {
+  padding: 13px 15px 12px 15px;
+  display: block;
+  cursor: pointer;
+  margin-bottom: 6px;
+}
+
+ul li:hover,
+ul li:focus {
+  background-color: #45484e;
+}
+
 </style>
